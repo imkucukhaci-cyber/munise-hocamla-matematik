761a0085aa8146fd2e4c5ac1ad1b220652ae4891
imkucukhaci-cyber <imkucukhaci@outlook.com.tr>
2026-01-10 12:35:50 +0300

    big update 1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'ders-takip-v12'; // Versiyonu v2 yaptık (önbellek tazelenmesi için)
+const cacheName = 'ders-takip-v13'; // Versiyonu v2 yaptık (önbellek tazelenmesi için)
 const staticAssets = [
   './',
   './index.html',
