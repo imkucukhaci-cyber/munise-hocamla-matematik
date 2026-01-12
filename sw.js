1530b633d5d084d3276d7aa967e298b2f852b7d5
imkucukhaci-cyber <imkucukhaci@outlook.com.tr>
2026-01-12 20:12:11 +0300

    2.56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'ders-takip-v19.51'; // Versiyonu v2 yaptık (önbellek tazelenmesi için)
+const cacheName = 'ders-takip-v19.52'; // Versiyonu v2 yaptık (önbellek tazelenmesi için)
 const staticAssets = [
   './',
   './index.html',
